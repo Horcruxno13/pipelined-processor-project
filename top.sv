0a438a2015c1b1b9afa26ce7713a9805abb7344e
Angad <gatecrash360@gmail.com>
2024-10-25 21:31:18 -0400

    added some FSM logic and Instantiated Fetcher in Top

--- top.sv
+++ top.sv
@@ -1,4 +1,8 @@
 `include "Sysbus.defs"
+`include "fetcher.sv"
+`include "pipeline_register.sv"
+`include "pipeline_reg_struct.svh"
+`include "write_back.sv"
 
 module top
 #(
@@ -61,13 +65,115 @@ module top
 
   logic [63:0] pc;
 
+  
+
+
+   // Instantiate InstructionFetcher with FD_ prefixed signal names
+   
+   logic F_fetch_enable;
+   logic F_fetch_ack;
+   logic F_program_counter;
+   logic F_target_address;
+   logic F_select_target;
+   logic F_instruction_out;
+   logic F_address_out;
+   logic F_fetcher_done;
+
+
+   logic F_instruction_out_next;
+   logic F_address_out_next;
+   
+    InstructionFetcher instructionFetcher (
+        .clk(clk),
+        .reset(reset),
+        .fetch_enable(F_fetch_enable),
+        .fetch_ack(F_fetch_ack),
+        .pc_current(m_axi_arready),
+        .target_address(m_axi_rvalid),
+        .select_target(F_select_target),
+        .instruction_out(F_instruction_out),
+        .address_out(F_address_out),
+        .fetcher_done(F_fetcher_done),
+    );
+
+    /*todo - write the FSM here @angad*/
+     //STATE DEFINITION SIGNALS
+   enum {
+    MAIN_IDLE_STATE = 3'b000,
+    MAIN_FETCH_STATE = 3'b001,
+    MAIN_DECODE_STATE = 3'b010,
+    MAIN_EXECUTE_STATE = 3'b011,
+    MAIN_MEMORY_STATE = 3'b100,
+    MAIN_WRITE_BACK_STATE = 3'b101
+   } main_current_state, main_next_state;
+
+
+  //next state selection logic
+  always_comb begin
+      case (current_state)
+          MAIN_IDLE_STATE: begin
+              // next_state = fetch_enable? FETCHER_REQUEST_STATE: FETCHER_IDLE_STATE;
+          end
+
+          MAIN_FETCH_STATE: begin
+              // next_state = cache_request_ready? FETCHER_WAIT_STATE: FETCHER_REQUEST_STATE;
+              next_state = F_fetcher_done? MAIN_DECODE_STATE: MAIN_FETCH_STATE;
+          end
+
+          MAIN_DECODE_STATE: begin
+              // next_state = cache_result_ready? FETCHER_DONE_STATE: FETCHER_WAIT_STATE;
+          end
+
+          MAIN_EXECUTE_STATE: begin
+              // next_state = fetch_ack? FETCHER_IDLE_STATE: FETCHER_DONE_STATE;
+          end
+
+          MAIN_MEMORY_STATE: begin
+              // next_state = fetch_ack? FETCHER_IDLE_STATE: FETCHER_DONE_STATE;
+          end
+
+          MAIN_WRITE_BACK_STATE: begin
+              // next_state = fetch_ack? FETCHER_IDLE_STATE: FETCHER_DONE_STATE;
+          end
+      endcase
+  end
+
+    //output variable update logic
+  always_comb begin
+      case (current_state)
+          MAIN_IDLE_STATE: begin
+              // next_state = fetch_enable? FETCHER_REQUEST_STATE: FETCHER_IDLE_STATE;
+          end
+
+          MAIN_FETCH_STATE: begin
+              // next_state = cache_request_ready? FETCHER_WAIT_STATE: FETCHER_REQUEST_STATE;
+          end
+
+          MAIN_DECODE_STATE: begin
+              // next_state = cache_result_ready? FETCHER_DONE_STATE: FETCHER_WAIT_STATE;
+          end
+
+          MAIN_EXECUTE_STATE: begin
+              // next_state = fetch_ack? FETCHER_IDLE_STATE: FETCHER_DONE_STATE;
+          end
+
+          MAIN_MEMORY_STATE: begin
+              // next_state = fetch_ack? FETCHER_IDLE_STATE: FETCHER_DONE_STATE;
+          end
+
+          MAIN_WRITE_BACK_STATE: begin
+              // next_state = fetch_ack? FETCHER_IDLE_STATE: FETCHER_DONE_STATE;
+          end
+      endcase
+  end
+
+
+
   always_ff @ (posedge clk)
     if (reset) begin
       pc <= entry;
-    end else begin
-      $display("Hello World!  @ %x", pc);
-      $finish;
-    end
+      current_state <= MAIN_IDLE_STATE;
+    end else if (current_state == MAIN_IDLE_STATE)
 
   initial begin
     $display("Initializing top, entry point = 0x%x", entry);
